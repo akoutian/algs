145350ba328901300912c3786da647856bda8af8
Anastasios Koutian <akoutian2@gmail.com>
2023-03-19 17:08:48 +0000

    init

---
+++ main.cxx
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "slstack.h"
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(void)
+{
+    const std::vector<std::string> v{"to", "be", "or", "not", "to", "be"};
+
+    SLStack<std::string> stack;
+
+    std::for_each(v.begin(), v.end(),
+                  [&](const auto &s)
+                  {
+                      stack.push(s);
+                      std::cout << stack.size() << " " << stack.top() << std::endl;
+                  });
+
+    while (!stack.empty())
+    {
+        std::cout << stack.size() << " " << stack.pop() << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ slstack.h
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <memory>
+#include <stdexcept>
+
+#pragma once
+
+template <typename T> class SLStack
+{
+  public:
+    SLStack() : m_size{} {};
+
+    bool empty()
+    {
+        return !m_top;
+    }
+
+    void push(const T &s)
+    {
+        auto first = std::make_shared<Node>();
+        first->s = s;
+        first->next = m_top;
+        m_top = first;
+        ++m_size;
+    }
+
+    T pop()
+    {
+        if (empty())
+        {
+            throw std::runtime_error("Empty stack.");
+        }
+
+        const auto result = m_top->s;
+        m_top = m_top->next;
+        --m_size;
+
+        return result;
+    }
+
+    size_t size()
+    {
+        return m_size;
+    }
+
+    const T &top()
+    {
+        if (empty())
+        {
+            throw std::runtime_error("Empty stack.");
+        }
+
+        return m_top->s;
+    }
+
+  private:
+    struct Node;
+    std::shared_ptr<Node> m_top;
+    size_t m_size;
+};
+
+template <typename T> struct SLStack<T>::Node
+{
+    T s;
+    std::shared_ptr<Node> next;
+};
