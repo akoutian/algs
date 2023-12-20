856928a2f1dffd5a9e0be113cbdc9aad754c5c3a
Anastasios Koutian <akoutian2@gmail.com>
2023-12-20 20:54:10 +0000

    Add max segment tree that supports range addition

---
+++ src/algs/segtree/include/segtree/add-max-segtree.hxx
@@ -0,0 +1,184 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include <cstddef>
+#include <vector>
+
+namespace segtree
+{
+
+// TODO: implement minimum segtree and sum segtree
+
+// The max segment tree stores values from a given std::vector. It allows updating the value at a
+// given index and finding the maximum element in a given interval [a, b]. Performance is
+// logarithmic.
+// TODO: implement range checks for methods that accept indices
+template <typename T> class add_max_segtree
+{
+  public:
+    // Build a max segment tree representing the contents of a vector.
+    add_max_segtree(const std::vector<T> &values) : add_max_segtree(values, values.size())
+    {
+    }
+
+    // Add a value to the whole range.
+    void add(T val)
+    {
+        add(1, 0, m_n - 1, 0, m_n - 1, val);
+    }
+
+    // Add a value to the value at a given position.
+    void add(size_t pos, T new_val)
+    {
+        add(1, 0, m_n - 1, pos, pos, new_val);
+    }
+
+    // Add a value to a given interval [lo, hi].
+    void add(size_t lo, size_t hi, T new_val)
+    {
+        add(1, 0, m_n - 1, lo, hi, new_val);
+    }
+
+    // Retrieve the value at a given position.
+    T get(size_t pos)
+    {
+        return get(1, 0, m_n - 1, pos);
+    }
+
+    // Find the maximum element.
+    T get_max()
+    {
+        return get_max(1, 0, m_n - 1, 0, m_n - 1);
+    }
+
+    // Find the maximum element in a given interval [lo, hi].
+    T get_max(size_t lo, size_t hi)
+    {
+        return get_max(1, 0, m_n - 1, lo, hi);
+    }
+
+    size_t size()
+    {
+        return m_n;
+    }
+
+  private:
+    // The segment tree is a full binary tree, therefore it contains at most nmax = 2^(hmax + 1) - 1
+    // nodes, where hmax is the maximum height, which will be floor(log2(n)), where n is the number
+    // of elements in the array the tree represents. Therefore, the maximum number of nodes will be
+    // less than 4*n, so we allocate this amount of memory.
+    // NOTE: this could be a waste of memory in some cases.
+    // TODO: optimise
+    add_max_segtree(const std::vector<T> &values, size_t n) : m_tree(4 * n), m_lazy(4 * n), m_n{n}
+    {
+        build(values, 1, 0, m_n - 1);
+    }
+
+    void build(const std::vector<T> &values, size_t v, size_t lo, size_t hi)
+    {
+        if (lo == hi)
+        {
+            m_tree[v] = values[lo];
+            return;
+        }
+
+        const auto mid = (hi + lo) / 2;
+        const auto left = 2 * v;
+        const auto right = left + 1;
+
+        build(values, left, lo, mid);
+        build(values, right, mid + 1, hi);
+
+        m_tree[v] = std::max(m_tree[left], m_tree[right]);
+    }
+
+    void push(size_t v)
+    {
+        const auto left = 2 * v;
+        const auto right = left + 1;
+
+        m_tree[left] += m_lazy[v];
+        m_lazy[left] += m_lazy[v];
+
+        m_tree[right] += m_lazy[v];
+        m_lazy[right] += m_lazy[v];
+
+        m_lazy[v] = 0;
+    }
+
+    void add(size_t v, size_t lo, size_t hi, size_t query_lo, size_t query_hi, T val)
+    {
+        if (query_lo > query_hi)
+        {
+            return;
+        }
+
+        if (lo == query_lo && hi == query_hi)
+        {
+            m_tree[v] += val;
+            m_lazy[v] += val;
+            return;
+        }
+
+        const auto mid = (hi + lo) / 2;
+        const auto left = 2 * v;
+        const auto right = left + 1;
+
+        push(v);
+
+        add(left, lo, mid, query_lo, std::min(mid, query_hi), val);
+        add(right, mid + 1, hi, std::max(mid + 1, query_lo), query_hi, val);
+
+        m_tree[v] = std::max(m_tree[left], m_tree[right]);
+    }
+
+    T get(size_t v, size_t lo, size_t hi, size_t pos)
+    {
+        if (lo == hi)
+        {
+            return m_tree[v];
+        }
+
+        const auto mid = (hi + lo) / 2;
+        const auto left = 2 * v;
+        const auto right = left + 1;
+
+        push(v);
+
+        if (pos <= mid)
+        {
+            return get(left, lo, mid, pos);
+        }
+
+        return get(right, mid + 1, hi, pos);
+    }
+
+    T get_max(size_t v, size_t lo, size_t hi, size_t query_lo, size_t query_hi)
+    {
+        if (query_lo > query_hi)
+        {
+            return std::numeric_limits<T>::min();
+        }
+
+        if (lo == query_lo && hi == query_hi)
+        {
+            return m_tree[v];
+        }
+
+        const auto mid = (hi + lo) / 2;
+        const auto left = 2 * v;
+        const auto right = left + 1;
+
+        push(v);
+
+        return std::max(get_max(left, lo, mid, query_lo, std::min(query_hi, mid)),
+                        get_max(right, mid + 1, hi, std::max(query_lo, mid + 1), query_hi));
+    }
+
+    std::vector<T> m_tree;
+    std::vector<T> m_lazy;
+    size_t m_n;
+};
+
+} // namespace segtree
---
+++ src/algs/segtree/test/add-max-segtree-test.cxx
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "segtree/add-max-segtree.hxx"
+
+#include <doctest/doctest.h>
+#include <numeric>
+
+TEST_CASE("Test method `get`")
+{
+    std::vector<int> values(7);
+    std::iota(values.begin(), values.end(), 0);
+    segtree::add_max_segtree st(values);
+
+    for (size_t ii{}; ii < values.size(); ++ii)
+    {
+        CHECK(st.get(ii) == values[ii]);
+    }
+}
+
+TEST_CASE("Test method `add`: 1 - update one element")
+{
+    std::vector<int> values(7);
+    segtree::add_max_segtree st(values);
+
+    st.add(2, 44);
+
+    for (size_t ii{}; ii < 2; ++ii)
+    {
+        CHECK(st.get(ii) == values[ii]);
+    }
+
+    CHECK(st.get(2) == 44);
+
+    for (size_t ii{3}; ii < values.size(); ++ii)
+    {
+        CHECK(st.get(ii) == values[ii]);
+    }
+}
+
+TEST_CASE("Test method `add`: 2 - update multiple elements")
+{
+    std::vector<int> values(7);
+    segtree::add_max_segtree st(values);
+
+    st.add(2, 44);
+    st.add(3, 44);
+    st.add(5, 23);
+
+    CHECK(st.get(0) == values[0]);
+    CHECK(st.get(1) == values[1]);
+    CHECK(st.get(2) == 44);
+    CHECK(st.get(3) == 44);
+    CHECK(st.get(4) == values[4]);
+    CHECK(st.get(5) == 23);
+    CHECK(st.get(6) == values[6]);
+}
+
+TEST_CASE("Test method `add`: 3 - large segtree")
+{
+    std::vector<int> values(1'000'000);
+    segtree::add_max_segtree st(values);
+
+    st.add(1'000, 800'000, 3);
+    st.add(100, 500'000, 8);
+
+    for (size_t ii{100}; ii < 1'000; ++ii)
+    {
+        CHECK(st.get(ii) == 8);
+    }
+
+    for (size_t ii{1'000}; ii <= 500'000; ++ii)
+    {
+        CHECK(st.get(ii) == 11);
+    }
+
+    for (size_t ii{500'001}; ii <= 800'000; ++ii)
+    {
+        CHECK(st.get(ii) == 3);
+    }
+}
+
+TEST_CASE("Test method `get_max`: 1 - all elements equal")
+{
+    std::vector<int> values(7);
+    segtree::add_max_segtree st(values);
+
+    const auto max = st.get_max();
+
+    CHECK(max == 0);
+}
+
+TEST_CASE("Test method `get_max`: 2 - all elements equal - subarrays")
+{
+    std::vector<int> values(7);
+    segtree::add_max_segtree st(values);
+
+    const auto max0 = st.get_max(0, 2);
+    CHECK(max0 == 0);
+
+    const auto max1 = st.get_max(4, 6);
+    CHECK(max1 == 0);
+}
+
+TEST_CASE("Test method `get_max`: 3 - unique max")
+{
+    std::vector<int> values(7);
+    std::iota(values.begin(), values.end(), 0);
+    segtree::add_max_segtree st(values);
+
+    CHECK(st.get_max() == 6);
+
+    st.add(1);
+
+    CHECK(st.get_max() == 7);
+}
+
+TEST_CASE("Test method `get_max`: 4 - unique max - subarrays")
+{
+    std::vector<int> values(7);
+    std::iota(values.begin(), values.end(), 0);
+    segtree::add_max_segtree st(values);
+
+    const auto max0 = st.get_max(0, 2);
+    CHECK(max0 == 2);
+
+    const auto max1 = st.get_max(4, 6);
+    CHECK(max1 == 6);
+}
+
+TEST_CASE("Test method `get_max`: 5 - multiple max")
+{
+    const std::vector<int> values{0, 7, 2, 7, 4, 5, 6};
+    segtree::add_max_segtree st(values);
+
+    const auto max = st.get_max();
+
+    CHECK(max == 7);
+}
+
+TEST_CASE("Test method `get_max`: 6 - multiple max - subarrays")
+{
+    const std::vector<int> values{0, 7, 2, 7, 4, 5, 6};
+    segtree::add_max_segtree st(values);
+
+    const auto max0 = st.get_max(0, 3);
+    CHECK(max0 == 7);
+
+    const auto max1 = st.get_max(4, 6);
+    CHECK(max1 == 6);
+}
+
+// TODO: "sliding window" tests would be cool
